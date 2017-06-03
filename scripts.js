54f20896034e28f2033660db682ae74396564fc7
Magdalena Michalska <magda@MacBook-Pro-Magdalena.local>
2017-06-04 00:13:24 +0200

    add scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var name = prompt('Enter your name'); // linear comment ;)
+alert('Welcome to my page ' + name); // I don't know what to write ;)
+console.log('Welcome to my page ' + name); /*
+                                             block comment ;)
+                                           */
